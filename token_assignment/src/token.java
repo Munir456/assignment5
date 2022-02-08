868d4997a5de77616a1d803903b419686b0358c5
Munir456 <83150680+Munir456@users.noreply.github.com>
2022-02-08 02:48:45 -0500

    Finished the assignment

---
+++ token_assignment/src/token.java
@@ -0,0 +1,32 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class token {
+    
+    public static void main(String[] args) {
+        System.out.println("String?");
+        
+        Scanner sc = new Scanner(System.in); 
+        
+        int[] nums = new int[3];
+        int i, loop; 
+        int sum=0;
+        int prod = 1;
+        
+        for (i=0; i<nums.length;i++) {
+            nums[i] = sc.nextInt();
+            sum+=nums[i];
+            prod*=nums[i];
+        }
+        System.out.println("Tokens:"); 
+        
+        for (int element: nums) {
+        	System.out.println(element);
+        }
+        
+        System.out.println("Sum:" + sum);
+        System.out.println("Product:" + prod);
+        
+    }
+    
+}
